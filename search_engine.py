08ef861a77a48271208e7117133699f1672064af
Aldo Lipani <aldo.lipani@gmail.com>
2020-04-07 18:11:58 +0200

    feat: tfidf

--- search_engine.py
+++ search_engine.py
@@ -1,11 +1,13 @@
 import math
-import numpy as np
+import pickle
+
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from collections import defaultdict
 
 tokenizer = RegexpTokenizer(r'\w+')
 stop_words = set(stopwords.words('english'))
+
 
 def preprocess(text):
     bag_of_words = defaultdict(int)
@@ -16,6 +18,7 @@ def preprocess(text):
             bag_of_words[token] += 1
     return bag_of_words
 
+
 def get_direct_index(documents):
     direct_index = {}
     for topic_id in documents:
@@ -23,6 +26,7 @@ def get_direct_index(documents):
             document = documents[topic_id][document_id]
             direct_index[(topic_id, document_id)] = preprocess(document)
     return direct_index
+
 
 def get_inverted_index(direct_index):
     dictionary = defaultdict(int)
@@ -36,13 +40,15 @@ def get_inverted_index(direct_index):
             inverted_index[term] += [(document_id, tf)]
             cl += tf
             dictionary[term] += tf
-            lengths[document_id] += tf 
+            lengths[document_id] += tf
     return dictionary, cl, lengths, inverted_index
 
-def language_model(tf, cf, cl, ld, mu = 100):
-    return (tf + mu * cf / cl)/(mu + ld);
 
-def search(query, dictionary, cl, lengths, inverted_index, mu = 100, n = 1):
+def language_model(tf, cf, cl, ld, mu=100):
+    return (tf + mu * cf / cl) / (mu + ld);
+
+
+def search(query, dictionary, cl, lengths, inverted_index, mu=100, n=1):
     query_bag_of_words = preprocess(query)
     query_bag_of_words = [item for item in query_bag_of_words.items() if item[0] in dictionary]
     results = defaultdict(float)
@@ -55,29 +61,12 @@ def search(query, dictionary, cl, lengths, inverted_index, mu = 100, n = 1):
             ld = lengths[post[0]]
             results[post[0]] += language_model(tf, cf, cl, ld, mu)
 
-    results = sorted(list(results.items()), key=lambda t:-t[1])
-    #print(results, results[n-1][0])
+    results = sorted(list(results.items()), key=lambda t: -t[1])
+    # print(results, results[n-1][0])
     res = []
     for i in results[:n]:
         res.append(i[0])
     return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-import pickle
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-from collections import defaultdict
 
 
 class PreProcessor:
@@ -154,61 +143,61 @@ class InvertedIndex(Picklable):
 
 
 class Scorer:
-    
+
     def __init__(self):
         self.args = {}
-    
+
     def score(self, tf, df, cf, cl, ld):
         pass
-    
+
     def set_arg(self, name, value):
         self.args[name] = value
-    
-    
+
+
 class TFIDF(Scorer):
-            
+
     def score(self, tf, df, cf, cl, ld):
         nD = self.args['D']
         return tf * math.log(nD / df)
 
 
 class BM25(Scorer):
-    
+
     def __init__(self, b, k1):
         super(BM25, self).__init__()
         self.b = b
         self.k1 = k1
-    
+
     def score(self, tf, df, cf, cl, ld):
         nD = self.args['D']
-        eld = cl/nD
+        eld = cl / nD
         norm = self.k1 * (1 - self.b + self.b * ld / eld)
         return tf / (tf + norm) * math.log((nD - df + 0.5) / (df + 0.5))
 
-    
+
 class DirichletSmoothingLM(Scorer):
-    
+
     def __init__(self, mu=100):
         super(DirichletSmoothingLM, self).__init__()
         self.mu = mu
-            
+
     def score(self, tf, df, cf, cl, ld):
         return (tf + self.mu * cf / cl) / (self.mu + ld)
 
 
 class Search:
 
-    def __init__(self, 
-                 inverted_index: InvertedIndex, 
-                 documents, 
-                 pre_processor: PreProcessor, 
+    def __init__(self,
+                 inverted_index: InvertedIndex,
+                 documents,
+                 pre_processor: PreProcessor,
                  scorer=DirichletSmoothingLM()):
         self.pre_processor = pre_processor
         self.inverted_index = inverted_index
         self.documents = documents
         self.scorer = scorer
         self.scorer.set_arg('D', len(documents))
-        
+
     def set_indices(self, inverted_index: InvertedIndex, documents):
         self.inverted_index = inverted_index
         self.documents = documents
